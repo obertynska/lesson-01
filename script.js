a13b1c7dbbe6876ecdb2980cb695b7d40ffe7e89
obertynska <obertynska11@gmail.com>
2020-01-23 20:39:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let money, income, addExpenses, deposit, mission, period;
+
+mission ="be happy!";
+
+alert(mission);
+
+console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, totam?");
