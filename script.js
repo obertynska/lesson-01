9e084c0b6fa8bd3b99b9c3bac0a043279dd7029e
obertynska <obertynska11@gmail.com>
2020-01-24 19:59:32 +0200

    home work 2

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ let money, income, addExpenses, deposit, mission, period;
 
 mission ="be happy!";
 
-alert(mission);
+/*alert(mission);*/
 
 console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, totam?");
+
+//1
+money = 3000;
+income = "rent";
+addExpenses = "lessons, english, restaurants";
+deposit = false;
+mission = 300000;
+period = 3;
+
+//2
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ` + period + ` месяцам` );
+console.log(`Цель заработать ` + mission + ` евро` );
+
+let arr = addExpenses.toLowerCase().split(", ");
+console.log(arr);
+
+let budgetDay = money / 30;
+console.log(budgetDay + ` eur`);
